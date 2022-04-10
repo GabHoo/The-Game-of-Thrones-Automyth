88d350fcd2b6f2daeca905e4d22989431bfb5735
Gabriel <you@example.com>
2022-04-10 02:59:58 +0200

    -

--- generator.py
+++ generator.py
@@ -1,9 +1,9 @@
 from rdflib import Graph, RDFS, RDF, URIRef, Namespace, Literal
 from owlrl import DeductiveClosure, RDFS_Semantics
 import random, sys
-
-
 import sys
+
+from scipy import rand
 
 
 def random_pick(ist_class):
@@ -37,9 +37,10 @@ def main(argv, arc):
     DeductiveClosure(RDFS_Semantics).expand(g)
 
     #getting the list of all properties that main class EVENT has. properties is a list of a basic properties
-    properties=[]
+    properties=[]#list of tuples, bc we need its range too
     for s,p,o in g.triples((None, RDFS.domain, sem.Event)):
-        properties.append(s)
+        range_p=g.value(predicate = RDFS.range, subject=s, any=False) #HOPEFULLY THERE IS NOT MROE THAN ONE RANGE FOR THOSE
+        properties.append((s,range_p))
     
 
     #Getting all subclasses, which are all the specific events
@@ -51,23 +52,38 @@ def main(argv, arc):
 
     #i_properties=properties #list with generic Event properties + upcoming specific event properties
 
+    hero=random_pick("http://semanticweb.cs.vu.nl/2009/11/sem/Actor") #defining the hero of the story
+
     print("list of subevents is:", subEvents)
     for i in subEvents:
         print("Considering event",i)
-
         #NOW we are considering one subevent at a time
+
+        #WE FIRST ADD THE ALREADY EXISTING INSTANCE TO THE STORY AND ITS TRIPLES(EVENT_N)
         instance_i = g.value(predicate = RDF.type, object=i, any=False)
         print("found istance of ", i,": ", instance_i)
         story += g.triples((instance_i, None, None))
         
+        #THAN WE INSTANCIATE AND ADD TO STORY those that are common to every event
+        
+        for (p,r) in properties:
+            print(p)
+            if(p==URIRef("http://semanticweb.cs.vu.nl/2009/11/sem/hasActor")):
+                print("!!!!\n---\nputting hero")
+                story.add((instance_i, p,hero ))
+            else:
+                story.add((instance_i, p,random_pick(r)))
+
+        #THIS IS TO INSTANCIATE AND ADD TO STORY THE TRIPLES SPECIFIC OF THAT EVENT i 
         for s,p,o in g.triples((None,RDFS.domain,i)): #takiing all the specific event properties and adding them one by one to the all property list
             print("       considering",s)
-            
+            allranges=[] #THIS IS THO HANDLE MULTIPLE RANGES, SEE THRETENEDELEMENT (CALL TO ADVENTURE) EXAMPLE TO UNDERSTAND
             for s1,p1,o1 in g.triples((s,RDFS.range,None)):
                 print("       range: ",o1) 
-                ist=random_pick(o1)
-                
-                story.add((instance_i, s,ist ))
+                allranges.append(o1)
+            
+            ist=random_pick(random.choice(allranges))   
+            story.add((instance_i, s,ist ))
 
     story.serialize("./story.ttl")
 
