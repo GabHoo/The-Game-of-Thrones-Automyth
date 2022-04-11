530b32493ea8788da2553c5011d4437617d6c737
Gabriel <you@example.com>
2022-04-11 21:55:15 +0200

    working story generator with random picks

--- generator.py
+++ generator.py
@@ -1,4 +1,4 @@
-from rdflib import Graph, RDFS, RDF, URIRef, Namespace, Literal
+from rdflib import Graph, RDFS, RDF, URIRef, Namespace, Literal,XSD
 from owlrl import DeductiveClosure, RDFS_Semantics
 import random, sys
 import sys
@@ -7,25 +7,36 @@ from scipy import rand
 
 
 def random_pick(ist_class):
+    
+    if ist_class==URIRef("http://www.w3.org/2001/XMLSchema#boolean"):
+        print("considering boooolean")
+        return(random.choice([Literal("true", datatype=XSD.boolean),Literal("false", datatype=XSD.boolean)]))
 
     g=Graph()
-    g.parse("./Event_ontology.ttl") 
-    sem = Namespace("http://semanticweb.cs.vu.nl/2009/11/sem/") #RIGHT NOW I HAD A BUNCH OF INSTANCES IN THIS TURTLE SO I AM USING THE SAME BUT IDEALLY HERE THERE WOULD BE THE OTHERO
-    list_c=[]
-    for char in g.subjects(RDF.type, sem.Actor):
-        list_c.append(char)
+    g.parse("./instances_MOCK.ttl") 
+    g.parse("./Event_ontology.ttl")
+    #DeductiveClosure(RDFS_Semantics).expand(g) #CANT REALLY INFER other wise its a mess bc Mentor is subclass of a lot of stuff for example
+
+    list_e=[]
+    
+    class_node=URIRef(str(ist_class))
     
 
-    x=ist_class.split('/')[-1]
+    for e in g.subjects(RDF.type, class_node):
+        list_e.append(e)
     
-    if x=='Actor':
-        return(random.choice(list_c)) 
-    elif x=='Location':
-       return(random.choice(list_c))
-    elif x=='Power':
-       return(random.choice(list_c)) #!!!! UNTIL THERE IS LIST OF POWERS
-    else:
-       return(random.choice(list_c)) 
+    #THis is because Mentor for example is a subclass of actor. Since there are no instance directly for mentor we look for its super class (Actor) and pick instance of it.
+    while( not list_e):
+        print("EMPTY LIST for", class_node)
+        class_node = g.value(predicate = RDFS.subClassOf, subject=URIRef(str(ist_class)), any=False)
+        
+        for e in g.subjects(RDF.type, class_node):
+            list_e.append(e)
+
+
+    print("class is ",class_node," list is: ",list_e)
+    return(random.choice(list_e)) 
+
 
 def main(argv, arc):
     
@@ -50,8 +61,6 @@ def main(argv, arc):
     
     story = Graph() #creates the graph of the story
 
-    #i_properties=properties #list with generic Event properties + upcoming specific event properties
-
     hero=random_pick("http://semanticweb.cs.vu.nl/2009/11/sem/Actor") #defining the hero of the story
 
     print("list of subevents is:", subEvents)
@@ -73,14 +82,14 @@ def main(argv, arc):
                 story.add((instance_i, p,random_pick(r)))
 
         #THIS IS TO INSTANCIATE AND ADD TO STORY THE TRIPLES SPECIFIC OF THAT EVENT i 
-        for s,p,o in g.triples((None,RDFS.domain,i)): #takiing all the specific event properties and adding them one by one to the all property list
+        for s,p,o in g.triples((None,RDFS.domain,i)): #takiing all the specific event properties 
             print("       considering",s)
             allranges=[] #THIS IS THO HANDLE MULTIPLE RANGES, SEE THRETENEDELEMENT (CALL TO ADVENTURE) EXAMPLE TO UNDERSTAND
             for s1,p1,o1 in g.triples((s,RDFS.range,None)):
                 print("       range: ",o1) 
                 allranges.append(o1)
             
-            ist=random_pick(random.choice(allranges))   
+            ist=random_pick(random.choice(allranges))   #pick one from the possible ranges
             story.add((instance_i, s,ist ))
 
     story.serialize("./story.ttl")
