77179b4fcade11a9588b90366b1228716fd32094
Gabriel <you@example.com>
2022-04-15 15:14:42 +0200

    removed "_" from labels (?)

--- add_labels.py
+++ add_labels.py
@@ -4,9 +4,9 @@ import re
 #s = 'HERO:upset a HERO:Feelings.'
 #print(re.findall(':', s))
 #print(re.findall(r'(:)(.*?)', s))
-myfile = open("/Users/teresa/Desktop/Story-Generator/instance.txt", "r")
+myfile = open("all__instances.txt", "r")
 myline = myfile.readlines()
-f = open("mydocument.txt", mode = "a")
+"""f = open("mydocument.txt", mode = "a")
 for line in myline:
     #print(line)
 
@@ -18,8 +18,8 @@ for line in myline:
     f.write(line)
     f.write(line2)
 
-
-f.close()
+"""
+#f.close()
 
 '''with open('/Users/teresa/Desktop/Story-Generator/instance.txt') as f:
     print(f)
@@ -30,3 +30,18 @@ f.close()
         o = k.split(":")
         line = line + "\n" + k[0] + "rdfs:label" + "" + o[1] + ""
         print(line)'''
+
+f=open("new_all_inst.txt",mode="a")
+for line in myline:
+
+    if "rdfs:label" in line and "_" in line:
+        
+        k = line.split(" ")
+        k[2]=k[2].replace("_"," ")
+
+        line2=k[0]+" "+k[1]+" "+k[2]
+        f.write(line2)
+    else:
+        f.write(line)
+
+f.close()
